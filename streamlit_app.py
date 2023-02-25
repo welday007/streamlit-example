26268c57cc445ac1222784ac8f9bbdce7e35c562
welday007 <welday007@gmail.com>
2023-02-25 22:39:38 +0000

    v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,65 +40,11 @@ first_run = False
 
 
 """
-# Best Options v2
+# Best Options v3
 
 TradingView [QQQ](https://www.tradingview.com/chart/HBL4nq9u/?symbol=NASDAQ%3AQQQ) [SPY](https://www.tradingview.com/chart/HBL4nq9u/?symbol=AMEX%3ASPY)
 [Fidelity](https://digital.fidelity.com/prgw/digital/login/full-page?AuthRedUrl=https://oltx.fidelity.com/ftgw/fbc/oftop/portfolio#positions)  [TD Ameritrade](https://www.tdameritrade.com/)  [Options Tracker](https://docs.google.com/spreadsheets/d/1t_SdI4zWFZBWoAH2MUEr4AQlL9BDu8M2cuK6_cG6SbA/edit#gid=1662549766)
 """
-
-
-@st.cache_data
-def get_data(file_path, modified_time):
-    st.text(f'getting file update {file_path} {modified_time}')
-
-    source = pd.read_pickle(file_path)
-    st.text(f'Done reading pickle {file_path} {modified_time}')
-
-    source["Time"] = pd.to_datetime(source["Time"])
-
-    #  Drop all quotes outside normal trading times
-    source = source[source.Time.dt.strftime('%H:%M:%S').between('09:30:00', '16:00:00')]
-    source = source[source.Time.dt.weekday.between(0, 4)]
-
-    st.text(f'Done getting file update {file_path} {modified_time}')
-    return source
-
-
-def cha_ching_sound():
-    html_string = """
-                <audio controls autoplay>
-                <source src="https://www.orangefreesounds.com/wp-content/uploads/2022/04/Small-bell-ringing-short-sound-effect.mp3" type="audio/mp3">
-                </audio>
-                """
-
-    sound = st.empty()
-    sound.markdown(
-        html_string, unsafe_allow_html=True
-    )  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
-    time.sleep(10)  # wait for 2 seconds to finish the playing of the audio
-    sound.empty()  # optionally delete the element afterwards
-
-
-def email(subject, body):
-    st.text('emailing ' + subject + ': ' + body)
-    cha_ching_sound()
-    sender_address = 'welday007@gmail.com'
-    # sender_pass = 'fmecvnbcyuvhqsdb'
-    sender_pass = 'hgphjhfsrionsxmy'
-    # receiver_address = '7047232563@vtext.com'
-    receiver_address = 'welday008@gmail.com, 7047232563@vtext.com'
-    # Setup the MIME
-    message = MIMEMultipart()
-    message['From'] = sender_address
-    message['To'] = receiver_address
-    message['Subject'] = subject + ': ' + body
-
-    session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
-    session.starttls()  # enable security
-    session.login(sender_address, sender_pass)
-    text = message.as_string()
-    session.sendmail(sender_address, receiver_address, text)
-    session.quit()
 
 
 from platform import python_version
