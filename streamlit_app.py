5da958261a1fbe65ee2e58e3c1e24cedb37b8227
welday007 <welday007@gmail.com>
2023-02-25 22:30:05 +0000

    use db

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,10 @@ except:
     from bokeh.models import HoverTool
     from IPython.core.display import HTML
     from ipywidgets import HBox, Layout, VBox, interactive
+    import sqlalchemy as db
+    from sqlalchemy import Table, Column, Integer, String, MetaData
+    from sqlalchemy import create_engine, select
+    from sqlalchemy_utils import database_exists, create_database
 
     hv.extension("bokeh")
 
@@ -118,31 +122,79 @@ except:
 st.write(datetime.datetime.now())
 
 try:
-    df_all_ATM = get_data(file_path, modified_time)
+    # df_all_ATM = get_data(file_path, modified_time)
+
+    # Defining the Engine
+    engine = db.create_engine('sqlite:///Best_ATM.db', echo=True)
+
+    # Create the Metadata Object
+    metadata = db.MetaData()
+
+    # Define the profile table
+
+    # database name
+    profile = db.Table(
+        'Best_ATM',
+        metadata,
+        db.Column('OptionSymbol', db.String, primary_key=True, nullable=False),
+        db.Column('Time', db.Date),
+        db.Column('BestCovRet', db.Float),
+        db.Column('BestPoP', db.Float),
+        db.Column('BestValue', db.Float),
+        db.Column('BestDelta', db.Float),
+        db.Column('Bid', db.Float),
+        db.Column('Change', db.Float),
+        db.Column('DaysToExp', db.Integer),
+        db.Column('Symbol', db.String),
+    )
+
+    # Create the profile table
+    metadata.create_all(engine)
+
+    best_atm_table = db.Table('Best_ATM', metadata, autoload=True, autoload_with=engine)
+
+    query = db.select([col for col in best_atm_table.columns])
+
+    connection = engine.connect()
+    result_proxy = connection.execute(query)
+    result_set = result_proxy.fetchall()
+    len(result_set)
+
+    query = (
+        db.select([col for col in best_atm_table.columns])
+        .filter(best_atm_table.c.Time >= from_date)
+        .order_by(best_atm_table.c.Time.desc())
+    )
+
+    connection = engine.connect()
+    result_proxy = connection.execute(query)
+    result_set = result_proxy.fetchall()
+    df_all_ATM = pd.read_sql_query(query, engine)
+
     st.dataframe(df_all_ATM)
 except:
     st.text(f"Error reading {file_path}")
 
 
-with st.expander('Spiral'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# with st.expander('Spiral'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+#     Point = namedtuple('Point', 'x y')
+#     data = []
 
-    points_per_turn = total_points / num_turns
+#     points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
 
-    st.altair_chart(
-        alt.Chart(pd.DataFrame(data), height=250, width=250)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q')
-    )
+#     st.altair_chart(
+#         alt.Chart(pd.DataFrame(data), height=250, width=250)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q')
+#     )
